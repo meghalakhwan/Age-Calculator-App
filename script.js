1ba5ea9caade8ab125553f53d194445b9718f5d9
meghalakhwan <meghalakhwan9@gmail.com>
2023-10-24 22:35:54 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
-
+let result = document.getElementById("result")
 function calculateAge(){
    let birthDate = new Date(userInput.value);
    
@@ -28,6 +28,16 @@ function calculateAge(){
    if(d2 >= d1){
     d3 = d2 - d1;
    }else{
-    
+    m3--;
+    d3 = getDaysInMonth(y1, m1) + d2 - d1;
    }
+   if(m3 < 0){
+      m3 = 11;
+      y3--;
+   }
+   result.innerHTML = `You are ${y3} years, ${m3} months, ${d3} days old`;
+}
+
+function getDaysInMonth(){
+   return new Date(year, month, 0).getDate();
 }
